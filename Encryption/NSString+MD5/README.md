1875d6bcf96783ad660b8ac051efb54ee15e0457
YIem <admin@yiem.net>
2016-12-09 11:51:55 +0800

    NSString+MD5 封装   ----- 上传！

---
+++ Encryption/NSString+MD5/README.md
@@ -0,0 +1,3 @@
+# 对于iOS MD5 加密的 封装！
+
+NSString+MD5  加密！ 
---
+++ Encryption/NSString+MD5/YIem_NSString+MD5.h
@@ -0,0 +1,13 @@
+//
+//  YIem_NSString+MD5.h
+//  kbwmsh
+//
+//  Created by YIem on 16/12/7.
+//  Copyright © 2016年 YIem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YIem_NSString_MD5 : NSObject
++(NSString *) md5: (NSString *) strText;
+@end
